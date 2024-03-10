14bf405e3492021d7a638c1c5d3d6c3140fadf9c
Piotr Arendt <nazcain@gmail.com>
2024-03-10 12:06:59 +0100

    Add NEtlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config"
 
+import netlify from "@astrojs/netlify"
+
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
@@ -7,24 +9,12 @@ export default defineConfig({
   },
   markdown: {
     shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
       theme: "github-light",
-      // Alternatively, provide multiple themes
-      // https://shiki.style/guide/dual-themes#light-dark-dual-themes
-      // experimentalThemes: {
-      // light: "github-light",
-      // dark: "github-dark",
-      // },
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
       langs: [],
-      // Enable word wrap to prevent horizontal scrolling
       wrap: true,
-      // Add custom transformers: https://shiki.style/guide/transformers
-      // Find common transformers: https://shiki.style/packages/transformers
       transformers: [],
     },
   },
+  output: "server",
+  adapter: netlify(),
 })
